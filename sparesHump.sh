70608ad9659e707c3030178551d7278e5be2a8c3
obidar <omidbidar4@gmail.com>
2022-08-16 14:10:29 +0100

    initial

--- sparesHump.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-#$ -l rmem=4G
-#$ -pe smp 4
-#$ -l h_rt=60:00:00
-#$ -cwd
-#$ -V
-singularity exec --bind $PE_HOSTFILE:$PE_HOSTFILE:ro /home/cop20ob/dafoam-main/singularity/DAFoam-fieldinversion-latest.simg /data/cop20ob/humpLES/sparsehl0p05/Allrun.sh
