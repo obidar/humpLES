979f9d92ee2abca17a27611f55cc1864e2c0f290
obidar <omidbidar4@gmail.com>
2022-09-23 18:45:19 +0100

    changed hpc run folder

--- uniform48.sh
+++ uniform48.sh
@@ -4,4 +4,4 @@
 #$ -l h_rt=60:00:00
 #$ -cwd
 #$ -V
-singularity exec --bind $PE_HOSTFILE:$PE_HOSTFILE:ro /home/cop20ob/dafoam-main/singularity/DAFoam-fieldinversion-clean.simg /data/cop20ob/humpLES/uniform48/Allrun.sh
+singularity exec --bind $PE_HOSTFILE:$PE_HOSTFILE:ro /home/cop20ob/dafoam-main/singularity/DAFoam-fieldinversion-clean.simg /fastdata/cop20ob/humpLES/uniform48/Allrun.sh
