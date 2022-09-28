373e7b33677b416b60a3bddf57f78339e354d660
obidar <omidbidar4@gmail.com>
2022-09-28 18:35:15 +0100

    initial

---
+++ 0p1Uniform.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#$ -l rmem=4G
+#$ -pe smp 4
+#$ -l h_rt=40:00:00
+#$ -cwd
+#$ -V
+singularity exec --bind $PE_HOSTFILE:$PE_HOSTFILE:ro /home/cop20ob/dafoam-main/singularity/DAFoam-fieldinversion-clean.simg /fastdata/cop20ob/humpLES/0p1Uniform/Allrun.sh
--- Allrun.sh
+++ Allrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/dafoamuser/dafoam/loadDAFoam.sh
-cd /fastdata/cop20ob/humpLES/0p05Uniform
+cd /fastdata/cop20ob/humpLES/0p1Uniform
 mpirun -np 4 python runScript.py > logOpt.txt & ./autoSyncToGitHub.sh
