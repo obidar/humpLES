4cce0943ce62b842d86814cd4f3114ed631d517b
obidar <omidbidar4@gmail.com>
2022-09-28 18:31:48 +0100

    initial

--- Allrun.sh
+++ Allrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/dafoamuser/dafoam/loadDAFoam.sh
-cd /fastdata/cop20ob/humpLES/nonUniCircle33
+cd /fastdata/cop20ob/humpLES/0p05Uniform
 mpirun -np 4 python runScript.py > logOpt.txt & ./autoSyncToGitHub.sh
