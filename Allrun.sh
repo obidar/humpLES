2e4b7b569cb0af355f6a5a3d0d610107ddf58329
obidar <omidbidar4@gmail.com>
2023-01-16 12:47:28 +0000

    initial

--- Allrun.sh
+++ Allrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/dafoamuser/dafoam/loadDAFoam.sh
-cd /data/cop20ob/humpLES/fullUxLES
+cd /data/cop20ob/humpLES/curvatureBasedAlpha0p1
 mpirun -np 4 python runScript.py > logOpt.txt & ./autoSyncToGitHub.sh
--- humpFullLES.sh
+++ humpFullLES.sh
@@ -4,4 +4,4 @@
 #$ -l h_rt=60:00:00
 #$ -cwd
 #$ -V
-singularity exec --bind $PE_HOSTFILE:$PE_HOSTFILE:ro /home/cop20ob/dafoam-main/singularity/DAFoam-fieldinversion-latest.simg /data/cop20ob/humpLES/fullUxLES/Allrun.sh
+singularity exec --bind $PE_HOSTFILE:$PE_HOSTFILE:ro /home/cop20ob/dafoam-main/singularity/DAFoam-fieldinversion-latest.simg /data/cop20ob/humpLES/curvatureBasedAlpha0p1/Allrun.sh
